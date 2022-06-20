2cf892b3616cc92a2d2876caee021a2b4347fd13
Admon <Admon@LAPTOP-SQQ66P9A>
2022-06-20 19:49:45 +0700

    20/6/2022.

--- LabBigSchool_MaiLatVien/Controllers/AttendancesController.cs
+++ LabBigSchool_MaiLatVien/Controllers/AttendancesController.cs
@@ -1,4 +1,5 @@
-﻿using LabBigSchool_MaiLatVien.Models;
+﻿using LabBigSchool_MaiLatVien.DTOs;
+using LabBigSchool_MaiLatVien.Models;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -18,11 +19,14 @@ namespace LabBigSchool_MaiLatVien.Controllers
             _dbContext = new ApplicationDbContext();
         }
         [HttpPost]
-        public IHttpActionResult Attend([FromBody] int courseId)
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
         {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendance already exits!");
             var attendance = new Attendance
             {
-                CourseId = courseId,
+                CourseId = attendanceDto.CourseId,
                 AttendeeId = User.Identity.GetUserId()
             };
             _dbContext.Attendances.Add(attendance);
--- LabBigSchool_MaiLatVien/Models/ApplicationUser.cs
+++ LabBigSchool_MaiLatVien/Models/ApplicationUser.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -13,6 +15,13 @@ namespace LabBigSchool_MaiLatVien.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        public ICollection Followers { get; set; }
+        public ICollection Followees { get; set; }
+        public ApplicationUser()
+        {
+            Followers = new Collection<Following>();
+            Followees = new Collection<Following>();
+        }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
